161699b1eff4e3c899c4614891b8881540b4cb25
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-12-19 14:39:12 -0300

    transação

---
+++ JDBC6/db/DbException.java
@@ -0,0 +1,9 @@
+package JDBC6.db;
+
+public class DbException extends RuntimeException{ //exceção 
+    
+    private static final long serialVersionUID = 1L;
+    public DbException(String msg){
+        super(msg);
+    }
+}
---
+++ JDBC6/db/DbIntegrityException.java
@@ -0,0 +1,10 @@
+package JDBC6.db;
+
+public class DbIntegrityException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+    public DbIntegrityException(String msg){
+        super(msg);
+    }
+    
+}
