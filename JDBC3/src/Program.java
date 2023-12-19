33db01ae7b1de2e82c0db33686e890667f01ef15
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-12-19 14:00:54 -0300

    Update Program.java

--- JDBC3/src/Program.java
+++ JDBC3/src/Program.java
@@ -19,7 +19,7 @@ public class Program {
         try {
             conn = DB.getConnection();
 
-            st = conn.prepareStatement(
+            /*st = conn.prepareStatement(
                 "INSERT INTO seller "
                 + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
                 + "VALUES "
@@ -30,7 +30,11 @@ public class Program {
             st.setString(2, "carl@gmail.com");
             st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
             st.setDouble(4, 3000.0);
-            st.setInt(5, 4);
+            st.setInt(5, 4); */
+
+            st = conn.prepareStatement(
+					"insert into department (Name) values ('D1'),('D2')", 
+					Statement.RETURN_GENERATED_KEYS);
             
             int rowsAffected = st.executeUpdate(); //executa o  comando para inserir o novo dado
             
@@ -48,9 +52,9 @@ public class Program {
         
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (ParseException e){
+        } /*catch (ParseException e){
             e.printStackTrace();
-        }
+        }*/
 
         finally {
             DB.closeStatement(st);
