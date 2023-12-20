d5c8e60788611e3c6d6ebcf3e85a7d6bdb8b6f36
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-12-20 15:27:18 -0300

    class Seller

--- Projeto/src/applcation/Program.java
+++ Projeto/src/applcation/Program.java
@@ -1,12 +1,17 @@
 package Projeto.src.applcation;
 
+import java.util.Date;
+
 import Projeto.src.model.entities.Department;
+import Projeto.src.model.entities.Seller;
 
 public class Program {
     
     public static void main(String[] args) {
         
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        System.out.println(seller);
     }
 }
